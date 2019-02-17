11af06c6a42d437026fe6ca61b31ee9e83b49252
Stefan Quest <stefan.quest@gmail.com>
2019-02-17 14:04:36 +0100

    added a few more elements to html & changed the approach in js

--- readme.md
+++ readme.md
@@ -7,3 +7,22 @@ Github it
 
 - advanced own initiative give it a front end and design it 
 and launch it 
+
+<!-- rein example 
+
+const down = fetch('https://swapi.co/api/people/1')
+
+down
+  .then(response => response.json())
+  .then(json => console.log(json))
+
+async function getLuke(){
+  const response = await fetch('https://swapi.co/api/people/1')
+  const json = await response.json()
+}
+
+getLuke(); -->
+
+things that revealed themselves while coding
+...it makes sense to actually create your html structure first instead of diving into your js immediately.
+this helps solidify your vision of end points for your project.
--- script.js
+++ script.js
@@ -1,27 +1,14 @@
 // 1,write a script in swapi to get home planets  
 //************************************************ */Done
-// fucntion called homeworld which if you request a person  it should make a request for that persons homeworld 
-// and post a picture of said homeworld.
+// function called homeworld which if you request a person  it should make a request for that persons homeworld 
+// and post a picture of said homeworld. - a bit of a challenge since images are not in the api 
 
 // 2,request a person from the star wars api and request data for all of thier vehicles 
 // Github it 
 
 // - advanced own initiative give it a front end and design it and launch it
 
-// rein example 
 
-// const down = fetch('https://swapi.co/api/people/1')
-
-// down
-//   .then(response => response.json())
-//   .then(json => console.log(json))
-
-// async function getLuke(){
-//   const response = await fetch('https://swapi.co/api/people/1')
-//   const json = await response.json()
-// }
-
-// getLuke();
 
 
 
@@ -39,10 +26,11 @@
 
 // // use reponse to fetch homeworld 
 // problem below as the js is grabbing the value before it is being input
-async function getHomeplanet() {
-  const userInput = 'leia';
-  // document.getElementById('person').value;
 
+
+
+async function homeWorld() {
+  const userInput = document.getElementById('person').value;
   console.log(userInput);
   const person = fetch(`https://swapi.co/api/people/?search=${userInput}`);
   
@@ -52,18 +40,28 @@ person
   .then( json=> {
     const data= json;
     const personObj = data.results;
-    console.log (personObj);
+    //console.log (personObj);
+    const name = personObj[0].name
     const homeworld = personObj[0].homeworld;
-    console.log(homeworld);
+    const vehicles = personObj[0].vehicles
+    const starships= personObj[0].starships
+    console.log (name,homeworld,vehicles,starships);
+    return {
+      name: name,
+      homeworld : homeworld,
+      vehicles: vehicles,
+      starships: starships }
   }
 
   )
-
- //.then(json => console.log(json))
- 
-
-//   const response = await fetch(`https://swapi.co/api/people/${userInput}.homeworld`);
-//   const json = await response.json();
  }
 
-getHomeplanet();
+  
+
+
+homeWorld();
+
+// this function displays all the varius strings in homeWorld object in the Ui  
+function displayToUI (object){
+//do something according to description 
+};
